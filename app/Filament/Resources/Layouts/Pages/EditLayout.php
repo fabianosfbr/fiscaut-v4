7a2b6c9c4cbda2188b3bb0d43883f83e1802cca9
Fabiano Fernandes <contato@fabianofernandes.adm.br>
2026-02-09 13:41:41 -0300

    feat(layouts): customize edit page title and tabs
    Add getTitle method to set custom page title and implement combined relation manager tabs with custom icon for improved UI consistency.

--- app/Filament/Resources/Layouts/Pages/EditLayout.php
+++ app/Filament/Resources/Layouts/Pages/EditLayout.php
@@ -2,9 +2,10 @@
 
 namespace App\Filament\Resources\Layouts\Pages;
 
-use App\Filament\Resources\Layouts\LayoutResource;
 use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
+use Illuminate\Contracts\Support\Htmlable;
+use App\Filament\Resources\Layouts\LayoutResource;
 
 class EditLayout extends EditRecord
 {
@@ -15,9 +16,24 @@ class EditLayout extends EditRecord
         return [];
     }
 
+    public function getTitle(): string | Htmlable
+    {
+        return 'Gerenciar Leiaute';
+    }
+
+
     protected function getRedirectUrl(): string
     {
-
         return $this->getResource()::getUrl('index');
+    }
+
+    public function hasCombinedRelationManagerTabsWithContent(): bool
+    {
+        return true;
+    }
+
+    public function getContentTabIcon(): ?string
+    {
+        return 'heroicon-m-cog';
     }
 }
