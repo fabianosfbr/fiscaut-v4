bc1d970b24674716ae4610aea02f201283c0047c
Fabiano Fernandes <contato@fabianofernandes.adm.br>
2026-03-20 13:31:52 -0300

    feat: migration to add fiscaut connector data in tenant database

---
+++ database/migrations/2026_03_20_125132_add_fiscaut_connector_url_to_tenants_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('tenants', function (Blueprint $table) {
+            $table->string('fiscaut_connector_url')->nullable()->after('id');
+            $table->string('fiscaut_connector_token')->nullable()->after('fiscaut_connector_url');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('tenants', function (Blueprint $table) {
+            $table->dropColumn(['fiscaut_connector_url', 'fiscaut_connector_token']);
+        });
+    }
+};
